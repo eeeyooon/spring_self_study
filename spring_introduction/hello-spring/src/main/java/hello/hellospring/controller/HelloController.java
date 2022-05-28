c077aa4c4e56f0e5185aeaaab382a6f301e82011
jiyoon <vywns9978@gmail.com>
2022-05-28 15:47:43 +0900

    05/28 스프링 입문 수업 정리

--- spring_introduction/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ spring_introduction/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -39,10 +39,16 @@ public class HelloController {
             Hello hello = new Hello();
             hello.setName(name);
             return hello;
+
         }
         static class Hello {
             private String name;
-
+            
+            //getter, setter > Java bean 규약 
+            //라이브러리같은데서 쓰거나 메소드로 접근
+            //>> 프로퍼티 접근 방식이라고 함.
+            
+            
             public String getName() {
                 return name;
             }
@@ -51,5 +57,12 @@ public class HelloController {
                 this.name = name;
             }
         }
-
+    
+        //05.28
+        //http://localhost:9090/hello-api?name=Spring!!!!!!
+        //json 방식
+        //HTML과 달리 열고 닫고를 하지 않아도 됨. 심플 >> 최근에는 json 방식으로 통일됨.
+        //스프링도 기본이 객체를 반환하고 Responsebody로 반환하는데 > json으로 반환하는게
+        //기본으로 설정됨.
+        //>> json 방식으로 사용하는게 요즘 트렌드
 }
